de4f90ab3aa45dcaf5c43c9e41efd5c1108dbc16
Sofie Christensen <sofiec510@gmail.com>
2023-08-17 16:04:10 -0400

    render all puppies

--- src/API/index.js
+++ src/API/index.js
@@ -1,20 +1,15 @@
-import { useState, useEffect } from "react";
 
-export default function fetchAllPuppies() {
-    const [puppies, setPuppies] = useState([]);
-    useEffect(() => {
-        async function fetchAllPuppies() {
-            try {
-                const response = await fetch(
-                    "https://fsa-puppy-bowl.herokuapp.com/api/2306-GHP-ET-WEB-FT-SF/players"
-                );
-                const result = await response.json();
-                setPuppies(result.data.players);
-                console.log(result.data.players)
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        fetchAllPuppies();
-    }, []);
+
+export default async function fetchAllPuppies() {
+  try {
+    const response = await fetch(
+      "https://fsa-puppy-bowl.herokuapp.com/api/2306-GHP-ET-WEB-FT-SF/players"
+    );
+    const result = await response.json();
+
+    return result.data.players;
+
+  } catch (error) {
+    console.error(error);
+  }
 }
--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,5 +1,26 @@
 import fetchAllPuppies from "../API/index";
+import { useEffect, useState } from "react";
 
 export default function AllPlayers() {
-  return fetchAllPuppies();
+  const [puppyData, setPuppyData] = useState([]);
+  async function makePuppiesUseful() {
+    let puppyArray = await fetchAllPuppies();
+    setPuppyData(puppyArray);
+  }
+  useEffect(() => {
+    makePuppiesUseful();
+  }, []);
+
+  console.log(puppyData);
+
+  return puppyData.map((puppy) => {
+    return (
+      <div key={puppy.id}>
+        <h4>ID: {puppy.id}</h4>
+        <h4>Name: {puppy.name}</h4>
+        <h4>Breed: {puppy.breed}</h4>
+        <img src={puppy.imageUrl} />
+      </div>
+    );
+  });
 }
